8754ead254625ff0f499a9631dad640401906570
stellarAlien <saifgaida@gmail.com>
2022-05-20 22:22:01 +0100

    404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 main module
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
@@ -16,7 +16,11 @@ def teardown_appcontext():
     """ Teardown app context function"""
     storage.close()
 
-
+@app.errorhandler(404):
+def not_found_error():
+    """return error in json"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+                   
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
